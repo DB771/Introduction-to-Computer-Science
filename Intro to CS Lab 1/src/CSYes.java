63d4dd478c958a4b5083f187bc88d7303f643a9f
Darren Bonifacio <77589293+DB771@users.noreply.github.com>
2023-09-21 17:04:02 -0400

    Add files via upload

---
+++ Intro to CS Lab 1/src/CSYes.java
@@ -0,0 +1,16 @@
+// ************************************************************
+// File Name: CSYes.java
+// Purpose: Prints an exciting message about computer science
+// ************************************************************
+
+public class CSYes {
+
+    // -------------------------------------------------
+    // The following main method prints an exciting
+    // message about computer science
+    // -------------------------------------------------
+    public static void main(String[] args) {
+        System.out.println("Computer Science, Yes!!!!\n" +
+                "=========================");
+    }
+}
---
+++ Intro to CS Lab 1/src/Poem.java
@@ -0,0 +1,12 @@
+public class Poem {
+
+    // -------------------------------------------------
+    // The following main method prints a nice poem
+    // -------------------------------------------------
+    public static void main(String[] args) {
+        System.out.println("Roses are red\n" +
+                "Violets are blue\n" +
+                "Sugar is sweet\n" +
+                "And so are you!\n");
+    }
+}
